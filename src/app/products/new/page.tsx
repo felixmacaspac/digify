b0fec3fd7c6d831b68c215d9e3ca337a62d3c552
kurt-ds <delos.santos.kurt.allen@gmail.com>
2024-12-06 02:36:50 +0800

    trying cloudinary for image upload

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -1,10 +1,10 @@
 'use client'
-import signIn from "@/firebase/auth/signIn";
 import { useRouter } from 'next/navigation';
 import { useState } from "react";
+import { CldUploadWidget } from 'next-cloudinary';
 
 function Page(): JSX.Element {
-  const [ email, setEmail ] = useState( '' );
+  const [ productCode, setProductCode ] = useState( '' );
   const [ password, setPassword ] = useState( '' );
   const router = useRouter();
 
@@ -13,16 +13,16 @@ function Page(): JSX.Element {
     event.preventDefault();
 
     // Attempt to sign in with provided email and password
-    const { result, error } = await signIn( email, password );
+    // const { result, error } = await signIn( email, password );
 
-    if ( error ) {
-      // Display and log any sign-in errors
-      console.log( error );
-      return;
-    }
+    // if ( error ) {
+    //   // Display and log any sign-in errors
+    //   console.log( error );
+    //   return;
+    // }
 
-    // Sign in successful
-    console.log( result );
+    // // Sign in successful
+    // console.log( result );
 
     // Redirect to the admin page
     // Typically you would want to redirect them to a protected page an add a check to see if they are admin or 
@@ -34,41 +34,38 @@ function Page(): JSX.Element {
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-full max-w-xs">
         <form onSubmit={handleForm} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h1 className="text-3xl font-bold mb-6 text-black">Sign In</h1>
+          <h1 className="text-3xl font-bold mb-6 text-black">NEW CAMERA</h1>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-              Email
+              Product Code
             </label>
             <input
-              onChange={( e ) => setEmail( e.target.value )}
+              onChange={( e ) => setProductCode( e.target.value )}
               required
-              type="email"
-              name="email"
-              id="email"
-              placeholder="example@mail.com"
+              type="text"
+              name="product_code"
+              id="product_code"
+              placeholder="ABC DEF"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              onChange={( e ) => setPassword( e.target.value )}
-              required
-              type="password"
-              name="password"
-              id="password"
-              placeholder="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
+
+          <CldUploadWidget signatureEndpoint="api/sign-image">
+            {({ open }) => {
+              return (
+                <button onClick={() => open()}>
+                  Upload an Image
+                </button>
+              );
+            }}
+          </CldUploadWidget>
+    
           <div className="flex items-center justify-between">
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white font-semibold py-2 rounded"
+              className="w-full bg-black-500 text-dark font-semibold py-2 rounded"
             >
-              Sign In
+              Upload
             </button>
           </div>
         </form>
